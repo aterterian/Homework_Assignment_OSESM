9f8ab44ac364b443f46956b99b358f4c14775f5e
Dominik Luther <domi.luther@gmail.com>
2024-04-07 14:41:23 +0200

    Added the second function and a requirements file

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import *
+
+tree_height = 4
+diamond_size = 6
+
+print_christmas_tree(tree_height)
+print_diamond(diamond_size)
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,22 +1,41 @@
 import math
+import random
 
 
 def print_christmas_tree(height):
     # Print a christmas tree in a choosable height
+    if height <= 1:
+        print("Leave the tree to grow a bit more!")
+    else:
+        for i in range(1, height + 1):  # Define iterations (rows) based on the inputted height
+            print(" " * (height - i), end="")  # Print the right amount of spaces in order to shape the tree form
+            for j in range(1, i + 1):  # Define the amount of iterations (stars) necessary for the specific row
+                if random.random() < 0.15:  # Probability of placing a festive ball
+                    print("o", end=" ")
+                else:
+                    print("*", end=" ")
+            print("")  # Next line
+        width = height + height - 1
+        stem_height = math.ceil(height * 1 / 6)
+        stem_width = math.ceil(height / 5)
+        for i in range(1, stem_height + 1):  # Define iterations (height) of the stem
+            print(" " * math.floor((width / 2) - stem_width + 1), end="")  # Print the right amount of spaces in order to center the stem
+            print("| " * stem_width)
 
-    for i in range(1, height + 1):  # Define iterations (rows) based on the inputted height
-        print(" " * (height-i), end="")  # Print the right amount of spaces in order to shape the tree form
-        for j in range(1, i + 1):  # Define the amount of iterations (stars) necessary for the specific row
-            print("*", end=" ")
-        print("")  # Next line
-    width = height + height-1
-    stem_height = math.ceil(height*1/6)
-    stem_width = math.ceil(height/5)
-    for i in range(1, stem_height+1):  # Define iterations (height) of the stem
-        print(" " * math.floor((width/2)-stem_width+1), end="")  # Print the right amount of spaces in order to center the stem
-        print("* " * stem_width)
 
-
-print_christmas_tree(5)
+def print_diamond(size, edge_symbol='*', inner_symbol=' '):  # Gives the option to change the diamonds appearence, and also sets a standard appearence
+    if size <= 1:
+        print("Why would you want such a small diamond?")
+    else:
+        for i in range(size):
+            if i == 0:
+                print(" " * (size - i - 1) + edge_symbol)
+            else:
+                print(" " * (size - i - 1) + edge_symbol + inner_symbol * (2 * i - 1) + edge_symbol)
+        for i in range(size - 2, -1, -1):
+            if i == 0:
+                print(" " * (size - i - 1) + edge_symbol)
+            else:
+                print(" " * (size - i - 1) + edge_symbol + inner_symbol * (2 * i - 1) + edge_symbol)
 
 
